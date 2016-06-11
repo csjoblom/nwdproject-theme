61c2eec87f1d1cdc98bc65a6ecc7876a44d0c705
Chris Sjoblom <chris.sjoblom@gmail.com>
2016-06-10 20:35:44 -0700

    modded stuff

--- header.php
+++ header.php
@@ -44,9 +44,6 @@
    <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="css/custom.css">
-
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
